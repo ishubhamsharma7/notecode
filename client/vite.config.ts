c948e2f39343a4f4200dbf84a2fc3ff25df02036
Shubham Sharma <shubhamsharma649@gmail.com>
2024-02-19 20:44:02 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.REACT_APP_RAPID_API_URL': JSON.stringify('https://judge0-ce.p.rapidapi.com/'),
-    'process.env.REACT_APP_RAPID_API_HOST': JSON.stringify('judge0-ce.p.rapidapi.com'),
-    'process.env.REACT_APP_RAPID_API_KEY': JSON.stringify('8319f5e6c8msh59f489d43b22befp1dc4b8jsnbb691d8afba6'),
-
   },
 })
