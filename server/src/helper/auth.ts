23b4ff408172821dc8af8da06cc6002d2d466bd1
ishubhamsharma7 <shubhamsharma649@gmail.com>
2024-02-23 13:07:50 +0530

    added spacing

--- server/src/helper/auth.ts
+++ server/src/helper/auth.ts
@@ -23,6 +23,7 @@ declare module 'express-serve-static-core' {
 
 export default async function auth(req:Request,res:Response,next:NextFunction){
     const userTokenCookie = req.cookies.token;
+    
     if(!userTokenCookie) return res.status(403).json({message:"no token"});
 
     try {
